c84abdfa9be9225743be90346da38aed892529fe
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-03-18 14:26:35 -0400

    main ≡ +| first commit. setup folder structure..

---
+++ SwiftUICoinApp/App/SwiftUICoinAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUICoinAppApp.swift
+//  SwiftUICoinApp
+//
+//  Created by Jose Martinez on 3/17/23.
+
+import SwiftUI
+
+@main
+struct SwiftUICoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ SwiftUICoinApp/Core/Home/Views/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  SwiftUICoinApp
+//
+//  Created by Jose Martinez on 3/18/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                // top movers VIEW
+                
+                // all coins VIEW
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
